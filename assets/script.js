ac607c7b78baa6c76bbdc9d2e15a031b2c233282
Moudi <mjaderi97@gmail.com>
2023-09-11 02:39:07 +1000

    Completed the project

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const arrowLinks = document.querySelectorAll(".icon-arrow");
+
+    arrowLinks.forEach(function (arrowLink) {
+        arrowLink.addEventListener("click", function () {
+            const nextSection = arrowLink.closest("section").nextElementSibling;
+            if (nextSection) {
+                nextSection.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");

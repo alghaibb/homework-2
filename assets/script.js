5833a1263d8758290363d56398950905889d0e11
Moudi <141513228+alghaibb@users.noreply.github.com>
2023-09-11 17:34:47 +1000

    Updated script.js
    Added comments for easy readability

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,16 @@
+// Wait for the DOM content to be fully loaded before executing code
 document.addEventListener("DOMContentLoaded", function () {
+    // Select all elements with the class "icon-arrow"
     const arrowLinks = document.querySelectorAll(".icon-arrow");
 
+    // Loop through each arrow link
     arrowLinks.forEach(function (arrowLink) {
+        // Add a click event listener to each arrow link
         arrowLink.addEventListener("click", function () {
+            // Find the next sibling section of the clicked arrow link's parent section
             const nextSection = arrowLink.closest("section").nextElementSibling;
+            
+            // If there is a next section, scroll to it smoothly
             if (nextSection) {
                 nextSection.scrollIntoView({
                     behavior: "smooth"
@@ -13,9 +20,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Function to toggle the mobile menu
 function toggleMenu() {
+    // Select the menu links container and the hamburger icon
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+
+    // Toggle the "open" class on the menu links container and the hamburger icon
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
